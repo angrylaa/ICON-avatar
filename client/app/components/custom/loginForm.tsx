800bfde680ec4b552c60ef4a3efca3db9a48a040
angela <114451995+angrylaa@users.noreply.github.com>
2025-08-01 12:58:34 -0400

    build homepage

---
+++ client/app/components/custom/loginForm.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "~/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  password: z.string(),
+});
+
+export function LoginForm() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <div className="flex flex-col gap-4">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem className="mb-4">
+                <FormControl>
+                  <Input placeholder="Username..." {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem className="mb-4">
+                <FormControl>
+                  <Input placeholder="Password..." {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button
+            className="bg-[#B4933F] hover:bg-[#947627] hover:cursor-pointer"
+            type="submit"
+          >
+            Login
+          </Button>
+        </form>
+        <div className="text-sm text-[#B4933F] hover:text-[#947627] hover:cursor-pointer">
+          Forgot Password?
+        </div>
+      </Form>
+    </div>
+  );
+}
--- client/app/routes/home.tsx
+++ client/app/routes/home.tsx
@@ -1,5 +1,7 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { LoginForm } from "~/components/custom/loginForm";
+import logo from "../assets/logo.png";
+import homeImg from "../assets/home-img.svg";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +11,35 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div className="flex h-screen">
+      <div className="w-1/2 h-full justify-center items-center flex">
+        <div className="w-sm mx-auto flex flex-col gap-18">
+          <img
+            src={logo}
+            alt="icon logo"
+            className="w-42 mx-auto block dark:hidden"
+          />
+          <div className="flex flex-col gap-4">
+            <div>
+              <div className="text-3xl font-bold text-[#B4933F]">
+                Welcome to Your AI Avatar
+              </div>
+              <div className="text-3xl font-bold text-[#B4933F]">
+                Sign into your account.
+              </div>
+            </div>
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+      <div className="w-1/2 h-full bg-[#CBB06A]">
+        <img
+          src={homeImg}
+          alt="home-page image"
+          className="w-full h-full object-cover"
+        />
+      </div>
+    </div>
+  );
 }
